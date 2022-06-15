0618cabc204b1cd1e869d089716c9fe2c5ca8c3e
John Murphy-Teixidor <johnvmt@gmail.com>
2022-06-15 13:49:37 -0400

    v0.0.2 - Updated to latest websockify-js

--- websockify.js
+++ websockify.js
@@ -3,12 +3,9 @@
 // Licensed under LGPL version 3 (see docs/LICENSE.LGPL-3)
 
 // Known to work with node 0.8.9
-// Requires node modules: ws, optimist and policyfile
-//     npm install ws optimist policyfile
+// Requires node modules: ws and optimist
+//     npm install ws optimist
 
-module.exports = function(options) {
-    return new Websockify(options);
-};
 
 var argv = require('optimist').argv,
     net = require('net'),
@@ -17,94 +14,49 @@ var argv = require('optimist').argv,
     url = require('url'),
     path = require('path'),
     fs = require('fs'),
-    policyfile = require('policyfile');
+    mime = require('mime-types'),
 
-var Buffer = require('buffer').Buffer,
-    WebSocketServer = require('ws').Server;
+    Buffer = require('buffer').Buffer,
+    WebSocketServer = require('ws').Server,
 
-function Websockify(options) {
-    var source_host, source_port;
+    webServer, wsServer,
+    source_host, source_port, target_host, target_port,
+    web_path = null;
 
-    var self = this;
-    // parse source and target arguments into parts
-    try {
-        var idx;
-        idx = options.source.indexOf(":");
-        if (idx >= 0) {
-            source_host = options.source.slice(0, idx);
-            source_port = parseInt(options.source.slice(idx+1), 10);
-        } else {
-            source_host = "";
-            source_port = parseInt(options.source, 10);
-        }
-
-        idx = options.target.indexOf(":");
-        if (idx < 0) {
-            throw("target must be host:port");
-        }
-        self.target_host = options.target.slice(0, idx);
-        self.target_port = parseInt(options.target.slice(idx+1), 10);
-
-        if (isNaN(source_port) || isNaN(self.target_port)) {
-            throw("illegal port");
-        }
-    } catch(e) {
-        console.error("websockify.js [--web web_dir] [--cert cert.pem [--key key.pem]] [source_addr:]source_port target_addr:target_port");
-        return null;
-    }
-
-    console.log("WebSocket settings: ");
-    console.log("    - proxying from " + source_host + ":" + source_port +
-    " to " + self.target_host + ":" + self.target_port);
-
-    if (options.web) {
-        console.log("    - Web server active. Serving: " + options.web);
-    }
-
-    if (options.cert) {
-        options.key = options.key || options.cert;
-        var cert = fs.readFileSync(options.cert),
-            key = fs.readFileSync(options.key);
-        console.log("    - Running in encrypted HTTPS (wss://) mode using: " + options.cert + ", " + options.key);
-        self.webServer = https.createServer({cert: cert, key: key}, self.http_request);
-    } else {
-        console.log("    - Running in unencrypted HTTP (ws://) mode");
-        self.webServer = http.createServer(self.http_request);
-    }
-    self.webServer.listen(source_port, function() {
-        self.wsServer = new WebSocketServer({server: self.webServer,
-            handleProtocols: self.selectProtocol});
-        self.wsServer.on('connection', function(client) { self.new_client(self, client) });
-    });
-
-// Attach Flash policyfile answer service
-    policyfile.createServer().listen(-1, self.webServer);
-
-
-}
 
 // Handle new WebSocket client
-Websockify.prototype.new_client = function(self, client) {
-
+new_client = function(client, req) {
     var clientAddr = client._socket.remoteAddress, log;
-    console.log(client.upgradeReq.url);
+    var start_time = new Date().getTime();
+
+    console.log(req ? req.url : client.upgradeReq.url);
     log = function (msg) {
         console.log(' ' + clientAddr + ': '+ msg);
     };
     log('WebSocket connection');
     log('Version ' + client.protocolVersion + ', subprotocol: ' + client.protocol);
 
-    var target = net.createConnection(self.target_port,self.target_host, function() {
+    if (argv.record) {
+        var rs = fs.createWriteStream(argv.record + '/' + new Date().toISOString().replace(/:/g, "_"));
+        rs.write('var VNC_frame_data = [\n');
+    } else {
+        var rs = null;
+    }
+
+    var target = net.createConnection(target_port,target_host, function() {
         log('connected to target');
     });
     target.on('data', function(data) {
         //log("sending message: " + data);
+
+        if (rs) {
+            var tdelta = Math.floor(new Date().getTime()) - start_time;
+            var rsdata = '\'{' + tdelta + '{' + decodeBuffer(data) + '\',\n';
+            rs.write(rsdata);
+        }
+
         try {
-            if (client.protocol === 'base64') {
-                client.send(new Buffer(data).toString('base64'));
-            } else {
-                client.send(data,{binary: true});
-            }
+            client.send(data);
         } catch(e) {
             log("Client closed, cleaning up target");
             target.end();
@@ -113,20 +65,29 @@ Websockify.prototype.new_client = function(self, client) {
     target.on('end', function() {
         log('target disconnected');
         client.close();
+        if (rs) {
+            rs.end('\'EOF\'];\n');
+        }
     });
     target.on('error', function() {
         log('target connection error');
         target.end();
         client.close();
+        if (rs) {
+            rs.end('\'EOF\'];\n');
+        }
     });
 
     client.on('message', function(msg) {
         //log('got message: ' + msg);
-        if (client.protocol === 'base64') {
-            target.write(new Buffer(msg, 'base64'));
-        } else {
-            target.write(msg,'binary');
+
+        if (rs) {
+            var rdelta = Math.floor(new Date().getTime()) - start_time;
+            var rsdata = ('\'}' + rdelta + '}' + decodeBuffer(msg) + '\',\n');
+            rs.write(rsdata);
         }
+
+        target.write(msg);
     });
     client.on('close', function(code, reason) {
         log('WebSocket client disconnected: ' + code + ' [' + reason + ']');
@@ -138,24 +99,45 @@ Websockify.prototype.new_client = function(self, client) {
     });
 };
 
+function decodeBuffer(buf) {
+    var returnString = '';
+    for (var i = 0; i < buf.length; i++) {
+        if (buf[i] >= 48 && buf[i] <= 90) {
+            returnString += String.fromCharCode(buf[i]);
+        } else if (buf[i] === 95) {
+            returnString += String.fromCharCode(buf[i]);
+        } else if (buf[i] >= 97 && buf[i] <= 122) {
+            returnString += String.fromCharCode(buf[i]);
+        } else {
+            var charToConvert = buf[i].toString(16);
+            if (charToConvert.length === 0) {
+                returnString += '\\x00';
+            } else if (charToConvert.length === 1) {
+                returnString += '\\x0' + charToConvert;
+            } else {
+                returnString += '\\x' + charToConvert;
+            }
+        }
+    }
+    return returnString;
+}
 
 // Send an HTTP error response
-Websockify.prototype.http_error = function (response, code, msg) {
+http_error = function (response, code, msg) {
     response.writeHead(code, {"Content-Type": "text/plain"});
     response.write(msg + "\n");
     response.end();
     return;
-};
+}
 
 // Process an HTTP static file request
-Websockify.prototype.http_request = function (request, response) {
+http_request = function (request, response) {
 //    console.log("pathname: " + url.parse(req.url).pathname);
 //    res.writeHead(200, {'Content-Type': 'text/plain'});
 //    res.end('okay');
 
-    var self = this;
     if (! argv.web) {
-        return self.http_error(response, 403, "403 Permission Denied");
+        return http_error(response, 403, "403 Permission Denied");
     }
 
     var uri = url.parse(request.url).pathname
@@ -163,7 +145,7 @@ Websockify.prototype.http_request = function (request, response) {
 
     fs.exists(filename, function(exists) {
         if(!exists) {
-            return self.http_error(response, 404, "404 Not Found");
+            return http_error(response, 404, "404 Not Found");
         }
 
         if (fs.statSync(filename).isDirectory()) {
@@ -172,24 +154,65 @@ Websockify.prototype.http_request = function (request, response) {
 
         fs.readFile(filename, "binary", function(err, file) {
             if(err) {
-                return self.http_error(response, 500, err);
+                return http_error(response, 500, err);
             }
 
-            response.writeHead(200);
+            var headers = {};
+            var contentType = mime.contentType(path.extname(filename));
+            if (contentType !== false) {
+                headers['Content-Type'] = contentType;
+            }
+
+            response.writeHead(200, headers);
             response.write(file, "binary");
             response.end();
         });
     });
 };
 
-// Select 'binary' or 'base64' subprotocol, preferring 'binary'
-Websockify.prototype.selectProtocol = function(protocols, callback) {
-    if (protocols.indexOf('binary') >= 0) {
-        callback(true, 'binary');
-    } else if (protocols.indexOf('base64') >= 0) {
-        callback(true, 'base64');
+function websockify(source_arg, target_arg, options = {}) {
+    var idx;
+    idx = source_arg.indexOf(":");
+    if (idx >= 0) {
+        source_host = source_arg.slice(0, idx);
+        source_port = parseInt(source_arg.slice(idx+1), 10);
     } else {
-        console.log("Client must support 'binary' or 'base64' protocol");
-        callback(false);
+        source_host = "";
+        source_port = parseInt(source_arg, 10);
     }
-};
+
+    idx = target_arg.indexOf(":");
+    if (idx < 0) {
+        throw("target must be host:port");
+    }
+    target_host = target_arg.slice(0, idx);
+    target_port = parseInt(target_arg.slice(idx+1), 10);
+
+    if (isNaN(source_port) || isNaN(target_port)) {
+        throw("illegal port");
+    }
+
+    console.log("WebSocket settings: ");
+    console.log("    - proxying from " + source_host + ":" + source_port +
+        " to " + target_host + ":" + target_port);
+    if (options.web) {
+        console.log("    - Web server active. Serving: " + argv.web);
+    }
+
+    if (options.cert) {
+        options.key = options.key || options.cert;
+        var cert = fs.readFileSync(options.cert),
+            key = fs.readFileSync(options.key);
+        console.log("    - Running in encrypted HTTPS (wss://) mode using: " + options.cert + ", " + options.key);
+        webServer = https.createServer({cert: cert, key: key}, http_request);
+    } else {
+        console.log("    - Running in unencrypted HTTP (ws://) mode");
+        webServer = http.createServer(http_request);
+    }
+    webServer.listen(source_port, function() {
+        wsServer = new WebSocketServer({server: webServer});
+        wsServer.on('connection', new_client);
+    });
+}
+
+module.exports = websockify;
